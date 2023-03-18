2a86af34b46c535e6112ca48390353b54d78a724
Lilyaliya <lilyaliya@inbox.ru>
2023-03-18 03:05:05 +0300

    created navbar menu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+menu.onclick=()=>{
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+}
