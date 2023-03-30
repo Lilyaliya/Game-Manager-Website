d87902d9904fe0faa7d5485a4625dd5500577a39
Lilyaliya <lilyaliya@inbox.ru>
2023-03-30 11:02:11 +0300

    deleted default scroll + added scrollbar on reading

--- js/main.js
+++ js/main.js
@@ -38,3 +38,12 @@ var swiper = new Swiper(".trending-content", {
       },
     },
   });
+
+// Скроллбар прочитаного
+window.onscroll = function(){mufunction()};
+function mufunction(){
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
