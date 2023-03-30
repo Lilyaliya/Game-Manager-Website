324b56eb2a5c35af9b318ab889c0ff47b06535a3
Lilyaliya <lilyaliya@inbox.ru>
2023-03-30 23:03:49 +0300

    corrected menu-icon display

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector('.menu');
 menu.onclick=()=>{
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
+    //menu.classList.remove('active');
     bell.classList.remove('active');
 }
 
