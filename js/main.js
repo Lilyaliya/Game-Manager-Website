97c49365420adae3ee8167b918fd930902c1832c
Lilyaliya <lilyaliya@inbox.ru>
2023-04-11 03:14:34 +0300

    added option to add new own games

--- js/main.js
+++ js/main.js
@@ -55,3 +55,52 @@ div.classList.add('game-content');
 let gamesContainer = document.querySelector('section.games.container');
 console.log(gamesContainer);
 gamesContainer.appendChild(div);
+// 2. добавили прослушку на кнопку
+let submitBtn = document.getElementById('btn');
+console.log(submitBtn);
+submitBtn.addEventListener('click', displayGameDetail);
+// 3. создадим функцию отображения единицы игры
+function displayGameDetail(){
+  let gName = document.getElementById('gameName').value;
+  console.log(gName);
+  var pathPreview = document.getElementById('photoPreview').value;
+  console.log(pathPreview);
+  var pathTeaser = document.getElementById('teaser').value;
+  console.log(pathTeaser);
+  // выбрали жанр игры
+  let genre = document.querySelector('input[name="genre"]:checked').value;
+  console.log(genre);
+  
+  pathPreview = pathPreview.toString();
+  pathTeaser = pathTeaser.toString();
+  var t = pathTeaser.split('/')[5];
+  var m = pathPreview.split('/')[5];
+  if (t){
+    console.log("Id-шечка на Тихона: " + t);
+  }
+  if (m){
+    console.log("Id-шечка на обложку: " + m);
+  }
+  // let doc  = document.createHTMLDocument(gName);
+  // let gStr = genre === 'Action'? "Экшн": genre === 'RPG'? "Шутер": genre === 'Card'?"Карты": "Другое";
+  let imgPath = genre === 'Экшн'? "action.jpg": genre === 'Шутер'? "rpg.jpg": genre === 'Карты'?"card.jpg": "other.jpg";
+  // <img src="https://drive.google.com/uc?export=view&id=${pathPreview}" alt=""> 
+  let html = `<div class="box">
+                <img src="img/${imgPath}" alt=""> 
+                <div class="box-text">
+                    <h2>${gName}</h2>
+                    <h3>${genre}</h3>
+                    <div class="rating-download">
+                        <div class="rating">
+                            <i class='bx bxs-star' ></i>
+                            <span>4.7</span>
+                        </div>
+                        <a href="download.html" class="box-btn"><i class='bx bxs-message-alt-x' ></i></a>
+                    </div>
+                </div>
+              </div>`;
+
+  div.insertAdjacentHTML('beforeend', html);
+}
+
+
